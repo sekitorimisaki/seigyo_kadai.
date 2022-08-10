9ecac2c36cf6e6df6fb2af65bfc001e0e3222fce
sekitorimisaki <s21c1069sh@s.chibakoudai.jp>
2022-08-10 18:35:41 +0900

    リストを作りましたが動的リストがあり実行が怖いのでまだ実行していません

---
+++ list.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+struct list
+{  
+    int data;
+    struct list *next;
+};
+
+struct list *search_tails(struct list *searched_list){//最後尾のlistまでたどる
+    struct list *tmp = NULL;
+    while(*searched_list->next != nullptr){
+    tmp = searched_list->next;
+    searched_list = tmp->next;
+    }
+    return searched_list;
+}
+void set_list(struct list *previous_list){//新しくlistを作る
+    previous_list->next = new list;
+}
+void delete_list(struct list *delete_list){//listをけす
+    delete delete_list;
+}
+
+int main(){
+    int data_size;
+    struct list *first_list , *main_list ;
+    std::cin >> data_size ;
+    //listを指定した数だけ呼び出す
+    for(int i=0;i<data_size;i++){
+        main_list = search_tails(first_list);
+        set_list(main_list);
+    }
+    first_list->data = 1;//整数をセット
+    for(int i=0;i<data_size;i++){
+        main_list = search_tails(first_list);
+        main_list->data = i+1;
+    }
+    for(int i=0;i<data_size;i++){//整数を表示
+        main_list = search_tails(first_list);
+        std::cout << main_list->data << std::endl;
+    }
+    for(int i=0;i<data_size;i++){//作ったlistを消す
+        main_list = search_tails(first_list);
+        delete_list(main_list);
+    }
+}
